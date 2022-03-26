80df42f6e4a074dc5bf02f01aa2d28953b99248c
Developer-Kabir <hakabir40@gmail.com>
2022-03-26 23:48:45 +0600

    final commit

--- src/components/Car/Car.js
+++ src/components/Car/Car.js
@@ -6,7 +6,6 @@ import './Car.css'
 
 const Car = ({car, addToCart}) => {
     const {name, Price, picture} = car;
-    console.log (car);
     return (
         <div className='product'>
                         <div className='product-img'>
